e66cc27b55f8e1bd2f0350e037c0601c792d4ae0
Brian Kim <bk@breadtech.com>
2015-11-07 13:19:09 -0500

    put the picker code in gdmi.py

--- gmdi.py
+++ gmdi.py
@@ -32,7 +32,7 @@ def ls_icons(cat):
   return list(set(y))
 
 def grep_icons(cat,s):
-  # get icons 
+  # get icons
   icons = ls_icons(cat)
   # rval
   y = []
@@ -74,3 +74,101 @@ def mk_icon_pixbuf(ic,color='white',sz=36):
     print 'gmdi: mk_icon_pixbuf: path: '+path+' does not exist'
     return None
   return y
+
+class GMDIPicker(gtk.VBox):
+
+  def __init__( self ):
+    #
+    # model
+    #
+
+    # range = curr*256 <> curr*256+255
+    # the current low end of the unicode table
+    self.q = ''
+    self.curr = 'action'
+    self.groups = ls_categories()
+    self.groups.sort()
+    self.combobox_model = gtk.ListStore(str)
+    for x in self.groups:
+      self.combobox_model.append([x])
+
+    #
+    # ui
+    #
+
+    #
+    # layout: combobox, table of unicode, control
+    gtk.VBox.__init__( self )
+
+    # top: selects a popular unicode range
+    self.combobox = gtk.ComboBox()
+    cell = gtk.CellRendererText()
+    self.combobox.pack_start(cell)
+    self.combobox.add_attribute(cell, 'text', 0)
+    self.combobox.set_model( self.combobox_model )
+    self.combobox.connect( 'changed', self.cb_changed )
+
+    # middle: scroll window w list inside it
+    self.scroll = gtk.ScrolledWindow()
+    self.lv = gtk.List()
+
+    self.scroll.add_with_viewport(self.lv)
+
+    # bottom: search bar
+    self.search = gtk.Entry()
+    self.search.set_icon_from_pixbuf( 0, mk_icon_pixbuf( 'action/search', 'black' ) )
+    self.search.connect( 'activate', self.activate )
+
+    # add components to vbox
+    self.pack_start( self.combobox, False)
+    self.pack_start( self.scroll, True)
+    self.pack_start( self.search, False)
+
+  def cb_changed( self, cb ):
+    # get index
+    i = cb.get_active()
+    # get the curr
+    self.curr = self.groups[i] if i > -1 else self.curr
+    # update
+    self.update()
+
+  def activate( self, v ):
+    self.q = self.search.get_text()
+    self.update()
+
+  def clear( self ):
+    for child in self.lv.get_children():
+      self.lv.remove(child)
+
+  def start( self ):
+    self.show_all()
+    self.combobox.set_active(0)
+
+  def update( self ):
+    self.clear()
+    # get the icons
+    model = ls_icons(self.curr)
+    model.sort()
+    # populate the list
+    for icon in model:
+      if len(self.q) > 0 and self.q not in icon:
+        continue
+      ic = mk_icon( '/'.join([self.curr,icon]), 'black', 18 )
+      label = gtk.Label(icon)
+      hbox = gtk.HBox(False,10)
+      hbox.pack_start(ic, False)
+      hbox.pack_start(label, True)
+      align = gtk.Alignment()
+      align.set_padding(10,10,10,10)
+      align.add(hbox)
+      item = gtk.ListItem()
+      item.add(align)
+      self.lv.add( item )
+    self.lv.show_all()
+
+if __name__ == "__main__":
+  w = gtk.Window()
+  w.set_default_size( 320, 480 )
+  w.add( GMDIPicker() )
+  w.show_all()
+  gtk.main()
